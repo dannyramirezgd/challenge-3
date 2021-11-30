06cd7cf95a8e0d22115b804705ab9c8cd27734b0
dannyrgd <danny.ramirezgd@gmail.com>
2021-11-29 19:37:38 -0500

    added random function generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,41 @@
 // Assignment code here
-//I'm so upset ...
+//const resultEl = document.getElementById('result');
+//const lengthEl = document.getElementById('length');
+//const uppercaseEl = document.getElementById('uppercase');
+//const lowercaseEl = document.getElementById('lowercase');
+//const numbersEl = document.getElementById('numbers');
+//const symbolsEl = document.getElementById('symbols');
+//const generateEl = document.getElementById('generate');
+//const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUppper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+var randomGenerateLetter = function () {
+  return Math.floor(Math.random() * 26);
+};
+var getRandomNumber = function () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+function getRandomLower() {
+  return String.fromCharCode(randomGenerateLetter() + 97);
+}
+function getRandomUppper() {
+  return String.fromCharCode(randomGenerateLetter() + 65);
+}
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomLower());
+console.log(getRandomUppper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +46,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
+function generatePassword() {
+  var passwordLength = prompt(
+    "Please enter the number of characters you want for your new password. It must be more than 8 but fewer than 128"
+  );
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
+  var upperCases = confirm("Do you want uppercases in your password?");
+  var special = confirm("Do you want special characters in your password?");
+}
+//I am so confused about this
+//why isn't it add to git???
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
