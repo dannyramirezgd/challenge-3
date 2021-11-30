918bdb76d058edb5c2393faacd167e9877d7cef8
dannyrgd <danny.ramirezgd@gmail.com>
2021-11-30 18:26:17 -0500

    added some arrays and random functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,68 @@
 // Assignment code here
-//const resultEl = document.getElementById('result');
-//const lengthEl = document.getElementById('length');
-//const uppercaseEl = document.getElementById('uppercase');
-//const lowercaseEl = document.getElementById('lowercase');
-//const numbersEl = document.getElementById('numbers');
-//const symbolsEl = document.getElementById('symbols');
-//const generateEl = document.getElementById('generate');
-//const clipboardEl = document.getElementById('clipboard');
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUppper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
 
 var randomGenerateLetter = function () {
   return Math.floor(Math.random() * 26);
 };
-var getRandomNumber = function () {
+let getRandomNumber = function () {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 };
-function getRandomLower() {
+let getRandomLower = function() {
   return String.fromCharCode(randomGenerateLetter() + 97);
 }
-function getRandomUppper() {
+let getRandomUpper = function() {
   return String.fromCharCode(randomGenerateLetter() + 65);
 }
-function getRandomSymbol() {
+let getRandomSymbol = function() {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(getRandomLower());
-console.log(getRandomUppper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
+const randomFunc = {
+lower: getRandomLower(),
+upper: getRandomUpper(),
+number: getRandomNumber(),
+symbol: getRandomSymbol(),
+}
+console.log(randomFunc);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//Defining generatePassword function
+function generatePassword() {
+  let password = "";
+  //prompting the user with password requirements
+  var length = prompt("Please enter the number of characters you want for your new password. Characters must be more than 8 but fewer than 128");
+    if (length < 8 || length > 128){
+      window.alert("Please input a number that is more than 8 but fewer than 128");
+      generatePassword();
+    }
+  //lowercase, uppercase, numbers, special characters
+  var lowerCase = confirm("Do you want lowercase characters in your password?");
+  var upperCase = confirm("Do you want uppercase characters in your password?");
+  var numbers = confirm("Do you want numeric characters in your password?");
+  var special = confirm("Do you want special characters in your password?");  
+  
+  const typesCount = lowerCase + upperCase + numbers + special;
+
+  const typesArr = [{lowerCase}, {upperCase}, {numbers}, {special}].filter(item => Object.values(item)[0]);
+  //validate input and at least one character type is being selected, can not return null
+  if(typesCount === 0){
+    window.alert("Please include one criteria for your password");
+    generatePassword();
+  }
+  //generate password
+
+  for (i = 0; i < parseInt(length); i += typesCount){
+    typesArr
+
+    };
+
+  //display generated password
+  //return generatedPassword;
+
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -46,17 +70,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
-function generatePassword() {
-  var passwordLength = prompt(
-    "Please enter the number of characters you want for your new password. It must be more than 8 but fewer than 128"
-  );
-  var numbers = confirm("Do you want numbers in your password?");
-  var lowerCases = confirm("Do you want lowercases in your password?");
-  var upperCases = confirm("Do you want uppercases in your password?");
-  var special = confirm("Do you want special characters in your password?");
-}
-//I am so confused about this
-//why isn't it add to git???
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
