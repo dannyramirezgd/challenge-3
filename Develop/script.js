38ba5cb2c8503436ade1ef1e93c6a4a4e2df75fc
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-01 16:22:39 -0500

    password is being generated as intended

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+
+//Create an object with all possible characters inside strings
 const keys = {
 lower: "abcdefghijklmnopqrstuvwxyz",
 upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -6,42 +8,60 @@ number: "0123456789",
 symbol: "!@#$%^&*(){}[]=<>/,.",
 }
 
-console.log(keys);
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 //Defining generatePassword function
 function generatePassword() {
-  let password = "";
-  //prompting the user with password requirements
+
+  //Begin by assigned a variable with no string 
+  var generatedPassword = "";
+
+  //prompt the user with password length first
   var length = prompt("Please enter the number of characters you want for your new password. Characters must be more than 8 but fewer than 128");
-    if (length < 8 || length > 128){
+
+    //make sure the password length is between 8 and 128
+    while (length < 8 || length > 128){
       window.alert("Please input a number that is more than 8 but fewer than 128");
-      generatePassword();
+      var length = prompt("Please enter the number of characters you want for your new password. Characters must be more than 8 but fewer than 128")
     }
-  //lowercase, uppercase, numbers, special characters
+    //notify the user of their choice in length
+    window.alert("Your password will have " + length + " characters.");
+
+  //prompt the user whether they want lowercase, uppercase, numbers, and/or special characters in their password
   var lowerCase = confirm("Do you want lowercase characters in your password?");
   var upperCase = confirm("Do you want uppercase characters in your password?");
   var numbers = confirm("Do you want numeric characters in your password?");
   var special = confirm("Do you want special characters in your password?");  
-  
-  const typesCount = lowerCase + upperCase + numbers + special;
 
-  const typesArr = [{lowerCase}, {upperCase}, {numbers}, {special}].filter(item => Object.values(item)[0]);
-  //validate input and at least one character type is being selected, can not return null
-  if(typesCount === 0){
+  //validate user input that at least one character type is being selected
+  while(!lowerCase && !upperCase && !numbers && !special){
     window.alert("Please include one criteria for your password");
-    generatePassword();
+    var lowerCase = confirm("Do you want lowercase characters in your password?");
+    var upperCase = confirm("Do you want uppercase characters in your password?");
+    var numbers = confirm("Do you want numeric characters in your password?");
+    var special = confirm("Do you want special characters in your password?");  
   }
-  //generate password
+  //create a string containing all the selected criteria by combining the strings from above based on user choice
+  var passwordCriteria = "";
+  if(lowerCase) {
+    passwordCriteria = passwordCriteria.concat(keys.lower);
+  };
+  if(upperCase){
+    passwordCriteria = passwordCriteria.concat(keys.upper);
+  };
+  if(numbers){
+    passwordCriteria = passwordCriteria.concat(keys.number);
+  };
+  if(special){
+    passwordCriteria = passwordCriteria.concat(keys.symbol);
+  };
 
-  for (i = 0; i < parseInt(length); i += typesCount){
+  //iterate a random character from the new string "passwordCriteria" as many times as the value of "length" and then return the value 
+  for (i = 0; i < length; i++){
+    var generatedPassword = generatedPassword + passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
     };
-
-  //display generated password
-  //return generatedPassword;
-
+return generatedPassword;
 };
 
 // Write password to the #password input
